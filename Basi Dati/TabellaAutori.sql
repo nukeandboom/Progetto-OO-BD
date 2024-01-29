094206a44ab5a69c362d912ca1b7be63c15e5ed2
nukeandboom <105439308+nukeandboom@users.noreply.github.com>
2024-01-29 12:59:16 +0100

    Add files via upload

---
+++ Basi Dati/TabellaAutori.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO "autore" ("username", "password", "data_iscrizione", "punteggio") VALUES ('Matteo02', 'matteo1', '2023-11-23', '0');
+INSERT INTO "autore" ("username", "password", "data_iscrizione", "punteggio") VALUES ('Il_Fede', 'fede1', '2023-12-30', '0');
+INSERT INTO "autore" ("username", "password", "data_iscrizione", "punteggio") VALUES ('JohnCena', 'John1', '2023-11-21', '0');
+INSERT INTO "autore" ("username", "password", "data_iscrizione", "punteggio") VALUES ('Bocchi', 'Therock', '2023-12-21', '0');
+INSERT INTO "autore" ("username", "password", "data_iscrizione", "punteggio") VALUES ('SolidSnake', 'Fox', '2024-01-12', '0');
---
+++ Basi Dati/TabellaPagina.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('Napoli', '2024-01-14', 'Il_Fede');
+INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('Kilimangiaro', '2024-01-15', 'Matteo02');
+INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('Martin Scorsese', '2024-01-16', 'JohnCena');
+INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('Tolkien', '2024-01-17', 'Bocchi');
+INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('Disarmamento nucleare', '2024-01-02', 'SolidSnake');
+INSERT INTO "pagina" ("titolo", "data_creazione", "nome_proprietario") VALUES ('SQL ', '2024-01-03', 'Matteo02');
